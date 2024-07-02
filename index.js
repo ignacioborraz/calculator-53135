2225d7e862cd284bfab96c305c707844e4e79843
igna <ignacioborraz@hotmail.com>
2024-07-01 21:50:18 -0300

    nuevas operaciones

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+import suma from "./operaciones/suma.js";
+import resta from "./operaciones/resta.js";
 import multiplicacion from "./operaciones/multiplicacion.js";
+import division from "./operaciones/division.js";
 
-const module = { multiplicacion };
-export { multiplicacion };
+const module = { suma, resta, multiplicacion, division };
+export { suma, resta, multiplicacion, division };
 export default module;
